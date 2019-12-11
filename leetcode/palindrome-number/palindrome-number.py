0f61314209c6b9d9981f989695032100507d11f2
Charles Dahab <charlesdahab@gmail.com>
2019-12-11 20:41:21 +0200

    Added Palindrome Number Algorithm Solution

--- leetcode/palindrome-number/palindrome-number.py
+++ leetcode/palindrome-number/palindrome-number.py
@@ -3,3 +3,17 @@
 # Palindrome Number Algorithm
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        temp = x
+        r = 0
+        while(temp != 0):
+            r = r * 10
+            r = r + int(temp % 10)
+            temp = int(temp/10)
+
+        if r == x:
+            return True
+
+        return False
+        
